9f7a1a7624f8fa6878b623c5eb08a776efcf78d1
Ana Beatriz dos Santos <61064552+anabeatrizds@users.noreply.github.com>
2021-03-10 17:10:47 -0300

    modulo b

---
+++ modulo b - operadores/info.js
@@ -0,0 +1,51 @@
+// comentario unica linha
+/* comentarios de
+mais de uma linha
+*/
+
+// "null" - sem nada - nulo
+ 
+
+//variaveis - espaço na memória do computador - guarda dados
+
+/*variaves tem identificadores, q são os nomes.. 
+- podem começar com letras,$ ou _ 
+- não podem começar com números 
+- é possivel usar letras ou numeros 
+- é possivel usar acentos ou simbolos
+- não pode ter espaço 
+- não pode ser palavras reservadas
+
+- maiuscula e miniscula faz diferença
+- use nomes coerentes
+- evite ser programadir alfabeto ou programador contador
+*/
+
+/* Data Types
+typeof - descobrir qual tipo
+
+tipo number -> 5   18  -12  0.5  -15.4  3.4 
+    -infinity - 
+    -NaN
+tipo string -> "google" 'Java' `ana`
+tipo boolean -> true / false
+tipo object - array
+tipo function
+tipo null
+tipo undefined
+*/
+
+// manipulação de dados
+
+// var e let
+
+
+/*   Exemplos números grandes, você digita e o js converte com ponto, virgula e cifrão
+
+        var n1 = 1545.5
+        n1.toFixed(2)    -> '1545,50'
+        n1.toFixed(2).replace('.',',') -> '1545,50'
+
+        n1.tolocalString('pt-BR', {style: 'currency', currency: 'BRL'})    -> 'R$1.545,50'
+        n1.tolocalString('pt-BR', {style: 'currency', currency: 'USD'})    -> 'US$1.545,50'
+*/
